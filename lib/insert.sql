adf9c82e4f45e5f1412b55ff9159619356351377
Mwagiru <joymwagiru@gmail.com>
2022-06-18 05:06:22 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 90, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 18, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 89, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 34, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 35, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 24, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 26, "M", "black", "aggressive", 0);
